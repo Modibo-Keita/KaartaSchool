dbe393aed4acffee87f881dd065a5c93def1d564
Modibo Keita <Keitavan.10@outlook.fr>
2025-11-30 22:23:29 +0000

    last fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  root: "client", // Le dossier frontend
+  root: "client",
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    tailwindcss(),
     metaImagesPlugin(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
@@ -26,13 +24,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"), // Alias pour ton code frontend
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "client/dist"), // Dossier attendu par Netlify
+    outDir: path.resolve(__dirname, "client/dist"),
     emptyOutDir: true,
   },
 });
